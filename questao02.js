a1ef8efdda2c2887a506dcac11f5148e76590dc6
camilyalves <camily10.alves2007@gmail.com>
2024-04-22 16:14:17 -0400

    add questao02

---
+++ questao02.js
@@ -0,0 +1,11 @@
+/*
+Exercícios de Operador Ternário:
+Questão: Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um aluno foi aprovado ou reprovado em um exame. A variável 
+nota contém a pontuação do aluno. Atribua o resultado à variável status
+*/
+
+var nota = 75; // ou qualquer outra pontuação
+var status = nota >= 60 ? "aprovado" : "reprovado";
+
+console.log("O aluno está " + status + ".");
