f2655fe71feea3523d9655a069da011226ddabe8
camilyalves <camily10.alves2007@gmail.com>
2024-04-22 16:19:27 -0400

    add questao12

---
+++ questao12.js
@@ -0,0 +1,12 @@
+/*
+Exercícios de Operador Ternário:
+Questão:   Escreva um código JavaScript utilizando o operador ternário para 
+determinar se uma pessoa pode votar. A variável idade contém a idade da 
+pessoa. Atribua o resultado à variável podeVotar 
+*/
+
+let idade = 18; // idade da pessoa
+
+let podeVotar = idade >= 16 ? "Pode votar" : "Não pode votar";
+
+console.log(podeVotar); // saída: Pode votar
