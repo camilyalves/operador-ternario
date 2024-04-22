36fb5387da6c20cd1f8fbde4a2651e9b11a66f22
camilyalves <camily10.alves2007@gmail.com>
2024-04-22 16:16:22 -0400

    add questao07

---
+++ questao07.js
@@ -0,0 +1,11 @@
+/*
+Exercícios de Operador Ternário:
+Questão:   Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um número é maior que 100. A variável numero contém o valor 
+a ser verificado. Atribua o resultado à variável resultado. 
+*/
+
+var numero = 120; // ou qualquer outro número
+var resultado = numero > 100 ? "é maior que 100" : "não é maior que 100";
+
+console.log("O número " + resultado + ".");
