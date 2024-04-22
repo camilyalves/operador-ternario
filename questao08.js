899115cfbe0420f4d71c04cae5a646caca51d867
camilyalves <camily10.alves2007@gmail.com>
2024-04-22 16:16:51 -0400

    add questao08

---
+++ questao08.js
@@ -0,0 +1,13 @@
+/*
+Exercícios de Operador Ternário:
+Questão:   Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um número é divisível por 3. A variável numero contém o valor a 
+ser verificado. Atribua o resultado à variável resultado. 
+*/
+
+var numero = 9; // ou qualquer outro número que deseje verificar
+var resultado = numero % 3 === 0 ? "é divisível por 3" : "não é divisível por 3";
+
+console.log("O número " + resultado + ".");
+
+
